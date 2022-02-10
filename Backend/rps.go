453ecac49dd7928cd82440ccd754650405713326
Atul Jairam <atul.jairam@stits.co>
2022-02-10 18:10:52 -0500

    working unique no generation

--- Backend/rps.go
+++ Backend/rps.go
@@ -7,7 +7,8 @@ import (
 )
 
 const (
-	
+	 ARRAY_SIZE=100 
+	 
 
 )
 
@@ -18,7 +19,7 @@ type Round struct {
 
 func PlayRound(playerValue int) Round {
 
-	fmt.Println(randomNoGenerator())
+	fmt.Println(uniqueGenerator())
 	var result Round
 	result.Winner = playerValue
 	
@@ -27,4 +28,38 @@ func PlayRound(playerValue int) Round {
 func randomNoGenerator()int{
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(9999-1000) + 1000
+}
+
+func digitValidator (number int) bool{
+	 firstNumber  := number / 1000;
+     secondNumber := (number / 100) - (firstNumber * 10);
+     thirdNumber  := (number % 100) / 10;
+     fourthNumber := number % 10;
+
+	 if ((firstNumber == secondNumber) || (firstNumber == thirdNumber) || (firstNumber == fourthNumber)) {
+		 return true;
+	 }else if ((secondNumber == thirdNumber) || (secondNumber == fourthNumber)) {
+		 return true;
+	 }else if (thirdNumber == fourthNumber) {
+		 return true;
+	 }
+ 
+	 return false;
+
+}
+
+func uniqueGenerator()int{
+	
+	var uniqueNumber int
+
+	for flag:=true;flag;flag=false  {
+		for i := 0; i < ARRAY_SIZE; i++ {
+			for digitValidator(uniqueNumber){
+					uniqueNumber=randomNoGenerator()
+			}
+			flag=true
+			
+		}
+	}
+	return uniqueNumber
 }
